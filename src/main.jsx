5bcbc03a28d0853a00cd21d45d25dfe0597dc172
Rodolfo Henrique <rodolfomarques27@gmail.com>
2026-05-30 21:05:02 -0300

    Remove tela branca no carregamento inicial

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const removerPreloaderInicial = () => {
+    const preloader = document.getElementById("app-preloader");
+
+    if (!preloader) return;
+
+    preloader.classList.add("app-preloader--hidden");
+
+    window.setTimeout(() => {
+        preloader.remove();
+    }, 220);
+};
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
+
+window.requestAnimationFrame(removerPreloaderInicial);
